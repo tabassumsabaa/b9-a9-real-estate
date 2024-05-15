9ca152e5d71a3b27c8cccfcad9557ccb76497b93
tabassumsabaa <sabihatabassum0511@gmail.com>
2024-05-15 21:35:04 +0530

    details page

--- src/Component/Estate/EstateDetails.jsx
+++ src/Component/Estate/EstateDetails.jsx
@@ -1,6 +1,8 @@
 
 import { useParams } from 'react-router-dom';
 import estatesData from '../../../public/estatesData.json';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
 
 function EstateDetails() {
   const { id } = useParams();
@@ -12,15 +14,29 @@ function EstateDetails() {
 
   return (
     <div>
-      <h1>Estate Details</h1>
-      <img src={estate.image} alt={estate.estate_title} />
-      <h2>{estate.estate_title}</h2>
-      <p>{estate.description}</p>
-      <p>Price: {estate.price}</p>
-      <p>Status: {estate.status}</p>
-      <p>Area: {estate.area}</p>
-      <p>Location: {estate.location}</p>
-      <p>Facilities: {estate.facilities.join(', ')}</p>
+      <div  className="w-3/4 mx-auto">
+        <Navbar></Navbar>
+        
+      <div className="my-20">
+      <h1 className="text-5xl font-bold text-center font-serif mb-10">Estate Details</h1>
+       <div className="hero min-h-screen bg-base-200"> 
+         <div className="hero-content flex-col lg:flex-row">
+           <img src={estate.image} alt={estate.estate_title} className="max-w-sm rounded-lg shadow-2xl" />
+          <div className="gap-2">
+          <h2 className="text-3xl font-bold mr-3 mb-5">{estate.estate_title}</h2>
+          <p className="text-3xl font-semibold mr-3">{estate.description}</p>
+          <p className="text-3xl font-semibold mr-3">Price: {estate.price}</p>
+          <p className="text-3xl font-semibold mr-3">Status: {estate.status}</p>
+          <p className="text-3xl font-semibold mr-3">Area: {estate.area}</p>
+          <p className="text-3xl font-semibold mr-3">Location: {estate.location}</p>
+          <p className="text-3xl font-semibold mr-3">Facilities: {estate.facilities.join(', ')}</p>             
+            <button to="/login" className="btn btn-secondary mt-5 ">Booking Here</button>
+          </div>
+         </div>
+       </div>
+      </div> 
+      </div>
+         <Footer></Footer>  
     </div>
   );
 }
